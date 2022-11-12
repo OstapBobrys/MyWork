c7c80a1480b589c47c7ccf4feec4dd0f1c878d2e
OstapBobrys <117922739+OstapBobrys@users.noreply.github.com>
2022-11-12 17:51:41 +0200

    Create aqua.sol

---
+++ AquaToken/aqua.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Aqua is ERC20 {
+
+    constructor(uint256 _supply) ERC20("Aqua", "AQUA") {
+        _mint(msg.sender, _supply * (10 ** decimals()));
+    }
+
+}
