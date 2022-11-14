86b6ef69466f0ebc9c3e5f16be34117f738c8700
OstapBobrys <117922739+OstapBobrys@users.noreply.github.com>
2022-11-14 17:22:22 +0200

    Update auction.sol

--- Auction/auction.sol
+++ Auction/auction.sol
@@ -3,8 +3,6 @@
 pragma solidity ^0.8.0;
 
 contract Auction {
-    // Повертає кошти тим, хто не переіг в аукціоні
-    // Таблиця лідерів
     address public owner;
     uint highestRate;
     address addressWinner;
