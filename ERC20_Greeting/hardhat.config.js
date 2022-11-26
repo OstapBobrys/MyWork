00855df3ca96141e0a0548361750cd0f74410aef
Ostap Bobrys <117922739+OstapBobrys@users.noreply.github.com>
2022-11-26 14:13:35 +0200

    Update hardhat.config.js

--- ERC20_Greeting/hardhat.config.js
+++ ERC20_Greeting/hardhat.config.js
@@ -11,8 +11,8 @@ module.exports = {
       chainId: 1337
     },
     goerli: {
-      url: `https://goerli.infura.io/v3/36c434c19a424afa99a5ca0929b3230d`,
-      accounts: [`0xfdf58643b07e511563dcbb42b8388c85c5f667160a4138d0aa32074f957c923b`]
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
     },
   }
 };
