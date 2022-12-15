2a17d666d53b1def56f604175a4041af78eacda2
Ostap Bobrys <117922739+OstapBobrys@users.noreply.github.com>
2022-12-15 21:50:23 +0200

    Add files via upload

---
+++ Bank-dapp/hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan")
+require("solidity-coverage")
+require("dotenv").config()
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  }
+};
