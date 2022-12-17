df58dbafd0266c591b6036828804b04cacd173ad
Ostap Bobrys <117922739+OstapBobrys@users.noreply.github.com>
2022-12-17 18:28:27 +0200

    Create Airdrop.sol

---
+++ Airdrop-NFT/contracts/Airdrop.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "./IERC721.sol";
+
+contract Airdrop {
+    address public immutable token;
+    bytes32 public immutable MerkleRoot;
+
+
+    mapping(address => bool) public claimed;
+    
+    event Claim(address indexed claimer);
+
+    constructor(address _token, bytes32 _merkleRoot) {
+        token = _token;
+        MerkleRoot = _merkleRoot;
+    }
+
+    function claim(bytes32[] calldata merkleProof) external {
+        require(canClaim(msg.sender, merkleProof), "Airdrop: Address is not a candidate for claim");
+        claimed[msg.sender] = true;
+        IERC721(token).safeMint(msg.sender);
+        emit Claim(msg.sender);
+    }
+
+    function canClaim(address claimer, bytes32[] calldata merkleProof) public view returns(bool) {
+        return !claimed[claimer] && MerkleProof.verify(
+            merkleProof,
+            MerkleRoot,
+            keccak256(abi.encodePacked(claimer))
+        );
+    }
+}
