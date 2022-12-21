42e78fdc2391ebf7b1e47eb5811e2058f64b1de7
Ostap Bobrys <117922739+OstapBobrys@users.noreply.github.com>
2022-12-21 13:51:05 +0200

    Update bank-test.js

--- Bank-dapp/test/bank-test.js
+++ Bank-dapp/test/bank-test.js
@@ -1,80 +1,73 @@
-const { expect } = require("chai")
-const { ethers } = require("hardhat")
-const { BigNumber } = require("ethers");
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
-describe("Bank", function () {
-  let owner
-  let bank
-  let maticContract
-  let shiba
-  let usdtContract
-  let user
-  let deployer
-  
-  const maticSymbol = ethers.utils.formatBytes32String('Matic')
-  const shibaSymbol = ethers.utils.formatBytes32String('Shib')
-  const usdtSymbol =  ethers.utils.formatBytes32String('Usdt')
+const str2Bytes32 = ethers.utils.formatBytes32String;
 
-  beforeEach(async function() {
-    [owner, user, deployer] = await ethers.getSigners()
+const deploy = async (name, deployer, ...args) => {
+  const factory = await ethers.getContractFactory(name, deployer);
+  const contract = await factory.deploy(...args);
+  await contract.deployed();
+  return contract;
+};
 
-    const Bank = await ethers.getContractFactory("Bank", owner)
-    bank = await Bank.deploy()
-    await bank.deployed()
+describe('Bank', function () {
+  let owner;
+  let Bank;
+  let Matic;
+  let Shib;
+  let Usdt;
+  let deployer;
 
-    const Matic = await ethers.getContractFactory("Matic", deployer);
-    maticContract = await Matic.deploy()
-    const Shib = await ethers.getContractFactory("Shib", deployer);
-    shiba = await Shib.deploy()
-    const Usdt = await ethers.getContractFactory("Usdt", deployer);
-    usdtContract = await Usdt.deploy()
+  beforeEach(async function () {
+    [owner, deployer] = await ethers.getSigners();
 
-    await maticContract.deployed()
-    await shiba.deployed()
-    await usdtContract.deployed()
+    Bank = await deploy('Bank', deployer);
+    Matic = await deploy('Matic', deployer);
+    Shib = await deploy('Shib', deployer);
+    Usdt = await deploy('Usdt', deployer);
 
+    const whitelist = [
+      { id: 'Matic', address: Matic.address },
+      { id: 'Shib', address: Shib.address },
+      { id: 'Usdt', address: Usdt.address },
+      { id: 'Eth', address: '0x4B7ee45f30767F36f06F79B32BF1FCa6f726DEda' },
+    ];
 
-    await bank.whitelistToken(
-      maticSymbol,
-      maticContract.address
-  );
-  await bank.whitelistToken(
-      shibaSymbol,
-      shiba.address
-  );
-  await bank.whitelistToken(
-      usdtSymbol,
-      usdtContract.address
-  );
-    })
+    const add2Whitelist = ({ id, address }) => Bank.whitelistToken(str2Bytes32(id), address);
 
-    it("Correct tokens data", async function() {
-      expect(await maticContract.name()).to.eq("Polygon")
-      expect(await maticContract.symbol()).to.eq("MATIC")
-      const symbols = await bank.whitlistedTokens(maticSymbol)
-      expect(symbols).to.eq(maticContract.address)
-      expect(await bank.whitelistedSymbols(0)).to.eq(maticSymbol)
+    await Promise.all(whitelist.map(add2Whitelist));
+  });
 
-      expect(await shiba.name()).to.eq("Shiba Inu")
-      expect(await shiba.symbol()).to.eq("SHIB")
-      const symbols2 = await bank.whitlistedTokens(shibaSymbol)
-      expect(symbols2).to.eq(shiba.address)
-      expect(await bank.whitelistedSymbols(1)).to.eq(shibaSymbol)
+  describe('Check tokens data', async function () {
+    it(`Correct Matic data`, async function () {
+      expect(await Matic.name()).to.eq('Polygon');
+      expect(await Matic.symbol()).to.eq('MATIC');
+    });
+    it(`Correct Shib data`, async function () {
+      expect(await Shib.name()).to.eq('Shiba Inu');
+      expect(await Shib.symbol()).to.eq('SHIB');
+    });
+    it(`Correct Usdt data`, async function () {
+      expect(await Usdt.name()).to.eq('Tether');
+      expect(await Usdt.symbol()).to.eq('USDT');
+    });
+  });
 
-      expect(await usdtContract.name()).to.eq("Tether")
-      expect(await usdtContract.symbol()).to.eq("USDT")
-      const symbols3 = await bank.whitlistedTokens(usdtSymbol)
-      expect(symbols3).to.eq(usdtContract.address)
-      expect(await bank.whitelistedSymbols(2)).to.eq(usdtSymbol)
-
-      const wlSymbols = await bank.getWhitelistedSymbols()
-      expect(wlSymbols[0]).to.eq(maticSymbol)
-      expect(wlSymbols[1]).to.eq(shibaSymbol)
-      expect(wlSymbols[2]).to.eq(usdtSymbol)
-
-      expect( await bank.getWhitelistedTokenAddress(maticSymbol)).to.eq(maticContract.address)
-      expect( await bank.connect(deployer).getTokenBalance(maticSymbol)).to.eq(5000)
-    })
-
-    
-})
+  describe('Check filled whitelist', async function () {
+    it(`Correct Matic address`, async function () {
+      const id = str2Bytes32('Matic');
+      expect(await Bank.whitelistedSymbols(0)).to.eq(id);
+      expect(await Bank.whitlistedTokens(id)).to.eq(Matic.address);
+    });
+    it(`Correct Shib data`, async function () {
+      const id = str2Bytes32('Shib');
+      expect(await Bank.whitelistedSymbols(1)).to.eq(id);
+      expect(await Bank.whitlistedTokens(id)).to.eq(Shib.address);
+    });
+    it(`Correct Usdt data`, async function () {
+      const id = str2Bytes32('Usdt');
+      expect(await Bank.whitelistedSymbols(2)).to.eq(id);
+      expect(await Bank.whitlistedTokens(id)).to.eq(Usdt.address);
+    });
+  });
+});
